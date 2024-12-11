81591d578182e5a5abbbdac40305fecefa6eff9f
Vinitha Kengatharan <87264797+Vini070@users.noreply.github.com>
2024-12-11 22:15:02 +0530

    Add InitialScreen

---
+++ lib/initial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child:Text('start screen',
+    style:TextStyle(color:Colors.white,fontSize:40),
+    ));
+  }
+}
