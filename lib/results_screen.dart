f26aa45feca90a4dfdae836b860c2d86d8cedba1
Vinitha Kengatharan <87264797+Vini070@users.noreply.github.com>
2024-12-11 22:15:23 +0530

    Add Results Screen

---
+++ lib/results_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child:Text(
+      'Results screen',
+    style:TextStyle(
+      color:Colors.white,
+    ),
+    ));
+  }
+}
