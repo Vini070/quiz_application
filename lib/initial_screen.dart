fbd1ae36476d973a994e95f1e7df632b40ee251d
Vinitha Kengatharan <87264797+Vini070@users.noreply.github.com>
2024-12-14 22:00:00 +0530

    InitialScreen UI completed.

--- lib/initial_screen.dart
+++ lib/initial_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class InitialScreen extends StatelessWidget {
   const InitialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child:Text('start screen',
-    style:TextStyle(color:Colors.white,fontSize:40),
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          'QUIZ TIME',
+          style: TextStyle(color: Colors.white, fontSize: 36),
+        ),
+        const Gap(37),
+        Image.asset(
+          'assets/images/quiz.png',
+          width: 300,
+          height: 300,
+        ),
+        const Gap(83),
+        SizedBox(
+          height: 51.19,
+          width: 303,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              'START QUIZ',
+              style: TextStyle(
+                color: Color(0xff01356B),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        )
+      ],
     ));
   }
 }
